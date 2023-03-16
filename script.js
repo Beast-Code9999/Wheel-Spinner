2de0245584dd10b635fdb04277327d8d60271b80
Beast Code <beastcode9999@gmail.com>
2023-03-16 15:49:03 +0900

    add WheelSpinner()

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+const WheelSpinner = (function WheelSpinner() {
+
+    const init = function init() {
+
+    }
+
+    return {
+        init: init
+    }
+})();
+
+$(document.ready(function() {
+    WheelSpinner.init();
+}));
