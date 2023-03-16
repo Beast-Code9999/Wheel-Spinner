a1a628dc537d19a84b4ecdceb15c8e92ad72e2d7
Beast Code <beastcode9999@gmail.com>
2023-03-16 16:34:37 +0900

    modified WheelCgildElem()

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const WheelSpinnerUI = (function WheelSpinnerUI() {
     const WheelChildElem = (function WheelChildElem() {
+        let _wheelNum = 6;
 
+        const _updateWheelNum = function _updateWheelNum() {
+            const select = document.getElementById('wheel-numbers');
 
-
+            $("#wheel-numbers").click(function(){
+                console.log(select.value);
+            })
+        }
 
 
         const updateWheel = function updateWheel() {
-            
-        }
+            _updateWheelNum();
+        }   
 
         return {
             updateWheel,
         }
     })();
 
+    const Fidget = (function Fidget() {
+
+        const updateFidget = function updateFidget() {
+
+        }
+
+        return {
+            updateFidget,
+        }
+    })();
 
     const init = function init() {
         WheelChildElem.updateWheel();
+        Fidget.updateFidget();
     }
     
     return {
